dac1bd7762f7308819dbd010532dbffbe89a2548
hemant <nhemnt@gmail.com>
2020-02-22 13:28:33 +0530

    add testing files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+  render() {
+    return (
+      <div data-test="component-app">
+      <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+      <button
+        data-test="increment-button"
+        onClick={() => this.setState({ counter: this.state.counter + 1 })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Increment counter
+
+      </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']")
+  console.log(appComponent);
 });
+
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
+
+});
+
